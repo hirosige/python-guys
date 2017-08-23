888416c468a44793bcd4c6d04bddc957451ab63b
AkihitoNakano <3.little.birds0824@gmail.com>
2017-08-23 11:45:51 +0900

    単語List
    単語のあるなし判定

---
+++ 003 ['りんご', 'ごりら', 'らっぱ']の配列を作って、'りんご'があったら「ある」、なかったら「ない」を表示/りんごごりららっぱ.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 23 11:07:35 2017
+
+@author: mysurface
+"""
+
+list_tango = ['りんご', 'ごりら', 'らっぱ']
+
+for i in list_tango:
+    
+    if 'りんご' == i:
+        print('ある') 
+    else:
+        print('ない')
+        
