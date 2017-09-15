4332be126edbdbd19491ef5a83c164c4f6b19c20
AkihitoNakano <3.little.birds0824@gmail.com>
2017-09-15 14:02:12 +0900

    ファイル分け

---
+++ 009 score_max/main.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 13 12:06:53 2017
+
+@author: mysurface
+"""
+
+import random
+import teacher
+import student
+        
+teacher = Teacher('花子')
+
+for i in range(100):
+
+    student = Student('student_' + str(i))
+    student.set_score(random.randint(1, 100))
+
+    teacher.add_student(student)
+
+print(teacher.get_name() + '先生の計算によると、')
+print('全体の人数は：{0}人'.format(len(teacher.students)))
+print('全体の平均は：{0}点'.format(teacher.calc_average()))
+print('最高得点は{0}点'.format(teacher.calc_scoremax()))
+print('でした。次も頑張って下さい')
