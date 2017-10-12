f00949c4aeae7d6a5801db2e7bbb2c4f2349e533
AkihitoNakano <3.little.birds0824@gmail.com>
2017-10-12 15:36:49 +0900

    ScoreJudge
    1回目作成

---
+++ 012 score_judge/judge.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 12 10:43:13 2017
+
+@author: mysurface
+"""
+
+class Judge:
+    def __init__(self, name):
+        self.name = name
+        self.standards = []
+
+    def add_standard(self, standard): 
+        self.standards.append(standard)
+        
+    def judge_score(self, score):
+        print(self.name)
+        exval = random.choice(self.standards)
+        if score < exval:
+            print('期待以下')
+            
+        elif score == exval:
+            print('期待通り')            
+            
+        else:
+            print('期待以上')
---
+++ 012 score_judge/main.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 12 10:24:42 2017
+
+@author: mysurface
+"""
+
+import random
+import judge
+
+score = input('テストの点数を入力して下さい。→')
+score = int(score)
+print(score)
+mens = []
+for i in range(10):
+    judgeman = Judge('Judgeman' + str(i))
+
+    standard = random.randint(0,100)
+    judgeman.add_standard(standard)
+    judgeman.judge_score(score)
+    mens.append(judgeman)
